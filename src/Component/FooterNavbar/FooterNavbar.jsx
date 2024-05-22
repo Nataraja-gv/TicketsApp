9c2cca8520d714c3e97906259b754773d2579811
nataraja <nataraja2k@gmail.com>
2024-05-22 13:06:02 +0530

    updating

--- src/Component/FooterNavbar/FooterNavbar.jsx
+++ src/Component/FooterNavbar/FooterNavbar.jsx
@@ -1,28 +1,60 @@
-import React from "react";
-import { GoHome } from "react-icons/go";
-import { GoBell } from "react-icons/go";
+import React, { useState } from "react";
+import { GoHome, GoBell } from "react-icons/go";
 import { PiBookLight } from "react-icons/pi";
 import { CgProfile } from "react-icons/cg";
 import "./FooterNavbar.css";
+import { Link } from "react-router-dom";
 
 const FooterNavbar = () => {
+  const [active, setActive] = useState("home");
+
+  const handleIconClick = (icon) => {
+    setActive(icon);
+  };
+
   return (
     <div className="footernavbar-main-container">
-      <div className="footer-home-conatiner">
-        <GoHome className="footer-navbar-icon" />
-        <h2>Home</h2>
+      <Link to="/">
+        <div
+          className="footer-home-container"
+          onClick={() => handleIconClick("home")}
+        >
+          <GoHome
+            className="footer-navbar-icon"
+            style={{ color: active === "home" ? "red" : "white" }} 
+          />
+          <h2 style={{ color: active === "home" ? "red" : "white" }}>Home</h2>
+        </div>
+      </Link>
+      <div
+        className="footer-notification-container"
+        onClick={() => handleIconClick("notifications")}
+      >
+        <GoBell
+          className="footer-navbar-icon"
+          style={{ color: active === "notifications" ? "red" : "white" }}
+        />
+        <h2 style={{ color: active === "notifications" ? "red" : "white" }}>Notifications</h2>
       </div>
-      <div className="footer-Notification-conatiner">
-        <GoBell className="footer-navbar-icon" />
-        <h2>Notifications</h2>
+      <div
+        className="footer-mybooking-container"
+        onClick={() => handleIconClick("bookings")}
+      >
+        <PiBookLight
+          className="footer-navbar-icon"
+          style={{ color: active === "bookings" ? "red" : "white" }}
+        />
+        <h2 style={{ color: active === "bookings" ? "red" : "white" }}>My Bookings</h2>
       </div>
-      <div className="footer-MyBooking-conatiner">
-        <PiBookLight className="footer-navbar-icon" />
-        <h2>My Bookings</h2>
-      </div>
-      <div className="footer-Profile-conatiner">
-        <CgProfile className="footer-navbar-icon" />
-        <h2>Profile</h2>
+      <div
+        className="footer-profile-container"
+        onClick={() => handleIconClick("profile")}
+      >
+        <CgProfile
+          className="footer-navbar-icon"
+          style={{ color: active === "profile" ? "red" : "white" }}
+        />
+        <h2 style={{ color: active === "profile" ? "red" : "white" }}>Profile</h2>
       </div>
     </div>
   );
